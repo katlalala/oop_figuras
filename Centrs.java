f409b8fa0b4709af17f805922d3db867c1c887d6
katlala <katrina.skudrite@sk.lvt.lv>
2025-09-23 11:20:31 +0300

    Pabeigta pirmā klase.

---
+++ Centrs.java
@@ -0,0 +1,24 @@
+package oop_figuras;
+
+public class Centrs {
+	
+		// ATRIBUTI
+		public int x, y;
+		
+		// KONSTRUKTORS
+		public Centrs (int x, int y) {
+			this.x = x;
+			this.y = y;
+			
+		}
+		
+		// METODES
+		public String izvaditKoordinatas() {
+			return "Centra punkts atrodas:\n x = " + x + " y = "+y;
+		}
+		
+		public void parvietot (int koordX, int koordY) {
+			x += koordX;
+			y += koordY;
+		}
+}
---
+++ Cetrsturis.java
@@ -0,0 +1,10 @@
+package oop_figuras;
+
+public class Cetrsturis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
